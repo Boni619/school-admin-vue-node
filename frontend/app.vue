92743231b50a06b2a0022b2087a57b21a527cc3d
Boni <boni619@gmail.com>
2023-08-13 23:30:24 +0530

    add: node api connect to frontend 😎

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
-    <NuxtWelcome />
+    {{ loginUser.user.name }}
   </div>
 </template>
+
+<script setup>
+const config = useRuntimeConfig();
+const url = config.public.baseUrl+'/auth/login';
+const userLoginData = {
+  email: 'john2@gmail.com',
+  password: 'password1',
+};
+
+const { data: loginUser } = await useFetch(url,{ method:'post',body: userLoginData})
+console.log(loginUser);
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    // The private keys which are only
+    // available within server-side
+    //apiSecret: process.env.API_SECRET,
+
+    // Keys within public, will be also be
+    // exposed to the client-side
+    public: {
+        baseUrl: process.env.API_URL,
+    },
+  },
 })
